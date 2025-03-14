da7edf859b8bfd2001bdabe36e42cd8d4ab7c236
Samuel Toillier <97335056+SleepwalkerGC@users.noreply.github.com>
2025-03-14 16:46:13 -0300

    Bug git resolvido

---
+++ Exec_1/Exec_1.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace Exec_1
+{
+    internal class Exec_1
+    {
+        static void Main(string[] args)
+        {
+            int INDICE = 13, SOMA = 0, K = 0;
+
+            while (K < INDICE)
+            {
+                K = K + 1;
+                SOMA = SOMA + K;
+            }
+
+            Console.WriteLine("O valor final de SOMA é: " + SOMA);
+            Console.ReadKey();
+        }
+    }
+}
